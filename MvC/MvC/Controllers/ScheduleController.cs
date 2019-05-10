dd00cb23eaf265ef6c1164ffe4ea402fdcd7917c
Katie Harris <katherineharris@ou.edu>
2019-05-09 23:19:13 -0500

    katie

---
+++ MvC/MvC/Controllers/ScheduleController.cs
@@ -0,0 +1,21 @@
+﻿using MvC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvC.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Schedule
+        public ActionResult Index(int year = 2019)
+        {
+            var date = db.FootballSchedules.Where(x => x.Season == year);
+            return View(date);
+        }
+    }
+}
